896642e85153f5d5e2b5e79e8100dda0893d4dc7
anthonypiazza <piazza.anthony.n@gmail.com>
2019-08-12 22:21:39 -0500

    updated with .then().catch() for endpoint requests

--- index.js
+++ index.js
@@ -2,33 +2,28 @@ const server = require('./server');
 
 const db = require('./dbConfig');
 
-server.get('/cars', async (req, res) => {
-    try{
-        const cars = await db('car-dealers');
-        res.json(cars);
-    }
-    catch(err){
-        res.status(500).json({ error: 'Couldnt get cars from DB' })
-    }
+server.get('/cars', (req, res) => {
+    
+    db('car-dealers')
+        .then(cars => {
+           res.status(200).json(cars); 
+        })
+        .catch(err => {
+           res.status(500).json({ error: 'Couldnt get cars from DB' }) 
+        })
 });
 
-server.post('/cars', async (req, res) => {
-    try{
-        const cars = await db('car-dealers').insert({
-            vin: "1IHJDUSY9P7IUHDGD",
-            make: "BMW",
-            model: "326i",
-            mileage: 90000,
-            //when turned integer^ to string it worked and converted auto to number
-            //when turning string to number it crashed
-            transmissionType: "automatic",
-            titleStatus: "clean"
-        });;
-        res.json(cars);
-    }
-    catch(err){
-        res.status(500).json({ error: 'Couldnt post cars from DB' })
-    }
+server.post('/cars', (req, res) => {
+    const newCar = req.body;
+
+    db('car-dealers')
+        .insert(newCar)
+        .then(newCarID => {
+            res.status(200).json(newCarID);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Couldnt post cars from DB' })
+        })
 });
 
 
